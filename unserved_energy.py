e819b9b22c72dd4a23ed39d8628d6b7e072cd105
Ana Dyreson <adyreson@wisc.edu>
2020-02-05 10:26:39 -0700

    unserved energy timeseries plot only non-zero

--- unserved_energy.py
+++ unserved_energy.py
@@ -53,7 +53,8 @@ class mplot(object):
             
         Unserved_Energy_Timeseries_Out = pd.DataFrame()
         Total_Unserved_Energy_Out = pd.DataFrame()    
-            
+        
+        
         for scenario in self.Multi_Scenario:
             unserved_eng_timeseries = Unserved_Energy_Collection.get(scenario)
             unserved_eng_timeseries = unserved_eng_timeseries.xs(self.zone_input,level=self.AGG_BY)
@@ -63,50 +64,56 @@ class mplot(object):
             Unserved_Energy_Timeseries_Out = pd.concat([Unserved_Energy_Timeseries_Out, unserved_eng_timeseries], axis=1, sort=False).fillna(0)
     
         Unserved_Energy_Timeseries_Out.columns = Unserved_Energy_Timeseries_Out.columns.str.replace('_',' ')     
-    
+        Unserved_Energy_Timeseries_Out = Unserved_Energy_Timeseries_Out.loc[:, (Unserved_Energy_Timeseries_Out >= 1).any(axis=0)]
         Total_Unserved_Energy_Out = Unserved_Energy_Timeseries_Out.sum(axis=0)
         
          # Data table of values to return to main program
         Data_Table_Out = Unserved_Energy_Timeseries_Out
+         
+        if Unserved_Energy_Timeseries_Out.empty==True:
+            print("no unserved energy in any scenario in "+self.zone_input)
+            
+            fig1, ax = plt.subplots(figsize=(9,6))
+
+            return {'fig': fig1, 'data_table': Data_Table_Out}
+        else:
+            fig1, ax = plt.subplots(figsize=(9,6))
         
-        fig1, ax = plt.subplots(figsize=(9,6))
+            # Converts color_list into an iterable list for use in a loop
+            iter_colour = iter(self.color_list)
     
-        # Converts color_list into an iterable list for use in a loop
-        iter_colour = iter(self.color_list)
-        
-        for column in Unserved_Energy_Timeseries_Out:
-            ax.plot(Unserved_Energy_Timeseries_Out[column], linewidth=3, antialiased=True, 
-                     color=next(iter_colour), label=column)
-            ax.legend(loc='lower left',bbox_to_anchor=(1,0), 
-                      facecolor='inherit', frameon=True)
-    
-        ax.set_ylabel('Unserved Energy (MW)',  color='black', rotation='vertical')
-        ax.set_ylim(bottom=0)
-        ax.spines['right'].set_visible(False)
-        ax.spines['top'].set_visible(False)
-        ax.tick_params(axis='y', which='major', length=5, width=1)
-        ax.tick_params(axis='x', which='major', length=5, width=1)
-        ax.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:,.0f}'))
-        ax.margins(x=0.01)
-        
-    #    ax.axvline(dt.datetime(2024, 1, 2, 2, 0), color='black', linestyle='--')
-    #    ax.axvline(outage_date_from, color='black', linestyle='--')
-    #    ax.text(dt.datetime(2024, 1, 1, 5, 15), 0.8*max(ax.get_ylim()), "Outage \nBegins", fontsize=13)
-    #    ax.axvline(dt.datetime(2024, 1, 6, 23, 0), color='black', linestyle='--')
-    #    ax.text(dt.datetime(2024, 1, 7, 1, 30), 0.8*max(ax.get_ylim()), "Outage \nEnds", fontsize=13)
-        
-        locator = mdates.AutoDateLocator(minticks=6, maxticks=12)
-        formatter = mdates.ConciseDateFormatter(locator)
-        formatter.formats[2] = '%d\n %b'
-        formatter.zero_formats[1] = '%b\n %Y'
-        formatter.zero_formats[2] = '%d\n %b'
-        formatter.zero_formats[3] = '%H:%M\n %d-%b'
-        formatter.offset_formats[3] = '%b %Y'
-        formatter.show_offset = False
-        ax.xaxis.set_major_locator(locator)
-        ax.xaxis.set_major_formatter(formatter)
-        
-        return {'fig': fig1, 'data_table': Data_Table_Out}
+            for column in Unserved_Energy_Timeseries_Out:
+                ax.plot(Unserved_Energy_Timeseries_Out[column], linewidth=3, antialiased=True, 
+                         color=next(iter_colour), label=column)
+                ax.legend(loc='lower left',bbox_to_anchor=(1,0), 
+                          facecolor='inherit', frameon=True)
+            ax.set_ylabel('Unserved Energy (MW)',  color='black', rotation='vertical')
+            ax.set_ylim(bottom=0)
+            ax.spines['right'].set_visible(False)
+            ax.spines['top'].set_visible(False)
+            ax.tick_params(axis='y', which='major', length=5, width=1)
+            ax.tick_params(axis='x', which='major', length=5, width=1)
+            ax.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:,.0f}'))
+            ax.margins(x=0.01)
+            
+        #    ax.axvline(dt.datetime(2024, 1, 2, 2, 0), color='black', linestyle='--')
+        #    ax.axvline(outage_date_from, color='black', linestyle='--')
+        #    ax.text(dt.datetime(2024, 1, 1, 5, 15), 0.8*max(ax.get_ylim()), "Outage \nBegins", fontsize=13)
+        #    ax.axvline(dt.datetime(2024, 1, 6, 23, 0), color='black', linestyle='--')
+        #    ax.text(dt.datetime(2024, 1, 7, 1, 30), 0.8*max(ax.get_ylim()), "Outage \nEnds", fontsize=13)
+            
+            locator = mdates.AutoDateLocator(minticks=6, maxticks=12)
+            formatter = mdates.ConciseDateFormatter(locator)
+            formatter.formats[2] = '%d\n %b'
+            formatter.zero_formats[1] = '%b\n %Y'
+            formatter.zero_formats[2] = '%d\n %b'
+            formatter.zero_formats[3] = '%H:%M\n %d-%b'
+            formatter.offset_formats[3] = '%b %Y'
+            formatter.show_offset = False
+            ax.xaxis.set_major_locator(locator)
+            ax.xaxis.set_major_formatter(formatter)
+            
+            return {'fig': fig1, 'data_table': Data_Table_Out}
 
         
     def tot_unserved_energy(self):
