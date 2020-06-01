c6d9642011540e15c19c6d069d87cd5e2db7d09d
Marty Schwarz <mschwarz@el2.ib0.cm.hpc.nrel.gov>
2020-06-01 13:50:04 -0600

    Align local and remote exevents branch.

--- run_html_output.R
+++ run_html_output.R
@@ -6,5 +6,5 @@ library(data.table)
 inputs = data.table(read.csv('Marmot_user_defined_inputs.csv', header = T))
 parent_dir = as.character(inputs[Input == 'Processed_Solutions_folder']$User_defined_value)
 rmarkdown::render(input = file.path('HTML_out.Rmd'),c('html_document'),
-                  output_file = paste0(parent_dir,'test.html'),output_dir = parent_dir)
+                  output_file = paste0(parent_dir,'.html'),output_dir = parent_dir)
 message(paste0('HTML compiled in ',Sys.time() - start_time,'minutes.'))
