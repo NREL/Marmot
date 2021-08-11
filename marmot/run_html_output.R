0961ed849f2efed40e3bebab26e7da12400ab78e
llavin13 <llavin13@users.noreply.github.com>
2021-08-11 15:23:33 -0600

    html updates

--- marmot/run_html_output.R
+++ marmot/run_html_output.R
@@ -3,8 +3,6 @@ start_time = Sys.time()
 library(data.table)
 inputs = fread('Marmot_user_defined_inputs.csv', header = T)
 parent_dir = as.character(inputs[Input == 'Marmot_Solutions_folder']$User_defined_value)
-print(parent_dir)
-print("hihdhs")
 agg_level = as.character(inputs[Input == 'AGG_BY']$User_defined_value)
 title = tail(strsplit(parent_dir,'/')[[1]], n = 1)
 rmarkdown::render(input = file.path('HTML_out.Rmd'),c('html_document'),
