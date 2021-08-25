c4af3f87de3db545a56cd9beff8ab01842c2b543
mschwarz <Marty.Schwarz@nrel.gov>
2021-08-25 15:00:56 -0600

    Add pandoc requirement to env file.

--- marmot/run_html_output.R
+++ marmot/run_html_output.R
@@ -1,11 +1,10 @@
 #Read in file locations
 start_time = Sys.time()
 library(data.table)
-inputs = fread('Marmot_user_defined_inputs.csv', header = T)
+inputs = fread('./Marmot_user_defined_inputs.csv', header = T)
 parent_dir = as.character(inputs[Input == 'Marmot_Solutions_folder']$User_defined_value)
-agg_level = as.character(inputs[Input == 'AGG_BY']$User_defined_value)
 title = tail(strsplit(parent_dir,'/')[[1]], n = 1)
-rmarkdown::render(input = file.path('HTML_out.Rmd'),c('html_document'),
+rmarkdown::render(input = file.path('./HTML_out.Rmd'),c('html_document'),
                   output_file = paste0(parent_dir,'.html'),output_dir = parent_dir,
                   output_options = list(pandoc_args = c(paste0('--metadata=title:',title))))
 message(paste0('HTML compiled in ',Sys.time() - start_time,'minutes.'))
