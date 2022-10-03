64b26e675bfb670c479c27db0474686efe14dc79
daniellevie <daniel.levie@nrel.gov>
2022-10-03 10:53:33 -0600

    adds DEFAULT_LOG_CONFIG
    -- will be used in event LOG_CONFIG_FILE not found

--- marmot/utils/loggersetup.py
+++ marmot/utils/loggersetup.py
@@ -4,7 +4,6 @@
 """
 
 from pathlib import Path
-import os
 import yaml
 import logging
 import logging.config
@@ -16,7 +15,7 @@ class SetupLogger:
 
     This class handles the following.
 
-    1. Configures logger from marmot_logging_config.yml file.
+    1. Configures logger from LOG_CONFIG_FILE file or DEFAULT_LOG_CONFIG.
     2. Handles rollover of log file on each instantiation.
     3. Sets log_directory.
     4. Append optional suffix to the end of the log file name
@@ -24,6 +23,42 @@ class SetupLogger:
     Optional suffix is useful when running multiple processes in parallel to
     allow logging to separate files.
     """
+
+    LOG_CONFIG_FILE: str = "marmot_logging_config.yml"
+    """Name of default logging config file located in utils package"""
+    
+    DEFAULT_LOG_CONFIG: dict =  {
+        'version': 1,
+        'formatters': {'info_format': {'format': '%(asctime)s:%(levelname)-8s- %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S'},
+        'warning_format': {'format': '%(asctime)s:%(levelname)s:%(module)s.%(funcName)s - %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S'}},
+        'handlers': {'console': {'class': 'logging.StreamHandler',
+            'level': 'INFO',
+            'stream': 'ext://sys.stdout'},
+        'warning_handler': {'class': 'logging.handlers.RotatingFileHandler',
+            'level': 'WARNING',
+            'formatter': 'warning_format',
+            'filename': '{}/WARNINGS_{}{}.log',
+            'backupCount': 3,
+            'encoding': 'utf8',
+            'mode': 'a'},
+        'info_handler': {'class': 'logging.handlers.RotatingFileHandler',
+            'level': 'INFO',
+            'formatter': 'info_format',
+            'filename': '{}/Log_{}{}.log',
+            'backupCount': 3,
+            'encoding': 'utf8',
+            'mode': 'a'}},
+        'loggers': {'formatter': {'level': 'INFO',
+            'handlers': ['console', 'warning_handler', 'info_handler'],
+            'propagate': True},
+        'plotter': {'level': 'INFO',
+            'handlers': ['console', 'warning_handler', 'info_handler'],
+            'propagate': True},
+        'root': {'level': 'DEBUG', 'handlers': ['console']}}
+    }
+    """Default log config if LOG_CONFIG_FILE cannot be found"""
 
     def __init__(
         self, logger_type: str, log_directory: Path = LOG_DIR, log_suffix: str = None
@@ -42,21 +77,21 @@ class SetupLogger:
         else:
             self.log_suffix = f"_{log_suffix}"
 
-        fromname = "marmot_logging_config.py"
-        toname = "marmot_logging_config.yml"
-        if not os.path.exists(ROOT_DIR.joinpath(os.path.join('utils', toname))):
-            os.rename(ROOT_DIR.joinpath(os.path.join("utils", fromname)), ROOT_DIR.joinpath(os.path.join("utils", toname)))
+        if not ROOT_DIR.joinpath("utils", self.LOG_CONFIG_FILE).exists():
+            conf = self.DEFAULT_LOG_CONFIG
 
-        with open(ROOT_DIR.joinpath(os.path.join("utils", "marmot_logging_config.yml")), "rt") as f:
-            conf = yaml.safe_load(f.read())
-            conf["handlers"]["warning_handler"]["filename"] = conf["handlers"][
-                "warning_handler"
-            ]["filename"].format(log_directory, logger_type, self.log_suffix)
-            conf["handlers"]["info_handler"]["filename"] = conf["handlers"][
-                "info_handler"
-            ]["filename"].format(log_directory, logger_type, self.log_suffix)
+        else:
+            with open(ROOT_DIR.joinpath("utils", self.LOG_CONFIG_FILE), "rt") as f:
+                conf = yaml.safe_load(f.read())
 
-            logging.config.dictConfig(conf)
+        conf["handlers"]["warning_handler"]["filename"] = conf["handlers"][
+            "warning_handler"
+        ]["filename"].format(log_directory, logger_type, self.log_suffix)
+        conf["handlers"]["info_handler"]["filename"] = conf["handlers"][
+            "info_handler"
+        ]["filename"].format(log_directory, logger_type, self.log_suffix)
+
+        logging.config.dictConfig(conf)
 
         self.logger = logging.getLogger(logger_type)
         # Creates a new log file for next run
