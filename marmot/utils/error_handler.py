ad20d0643936f3e44a2ff88bca8f1e0f5ebc7dad
daniellevie <daniel.levie@nrel.gov>
2022-10-21 12:22:35 -0600

    add error_handler module

---
+++ marmot/utils/error_handler.py
@@ -0,0 +1,42 @@
+
+from dataclasses import dataclass
+import pandas as pd
+
+@dataclass
+class ReEDSColumnLengthError(ValueError):
+    """Raised when there is a length mismatch between ReEDS df and ReEDSPropertyColumns"""
+
+    df: pd.DataFrame
+    prop_columns: list
+    prop: str
+    class_instance: str
+    
+    def __post_init__(self) -> None:
+        self.message = (f"ReEDS df has {len(self.df.columns)} columns, "
+                f"trying to assign {len(self.prop_columns)} elements for "
+                f"property {self.prop}.\n\n"
+                f"'{self.prop}' DataFrame has the following format:\n{self.df.head(5)}\n\n"
+                "Trying to apply the following column names:\n"
+                f"{self.prop_columns}\n\n"
+                f"Adjust the {self.prop} field values in {self.class_instance} class")
+
+    def __str__(self) -> str:
+        return self.message
+
+@dataclass
+class ReEDSYearTypeConvertError(ValueError):
+    """Raised when ReEDS df.year column cannot be converted to type int"""
+
+    df: pd.DataFrame
+    prop: str
+    class_instance: str
+
+    def __post_init__(self) -> None:
+        self.message = ("year column cannot be converted to type int.\nThis is likely due to an " 
+                    f"incorrectly ordered {self.class_instance} variable, for property '{self.prop}'.\n\n"
+                    f"{self.prop} DataFrame has the following format:\n{self.df.head(5)}\n\n"
+                    f"Check column order in {self.class_instance} and try running the formatter again.\n"
+                    f"If the issue persists open a GitHub issue.")
+
+    def __str__(self) -> str:
+        return self.message
