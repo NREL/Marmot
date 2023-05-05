7219f4c624b46430e20a31d4a187b1dd0b890b18
Micah Webb <105076850+micahpw-nrel@users.noreply.github.com>
2023-05-05 17:38:14 -0600

    integration of marmot quickplots, datahelpers, and scenariohandlers. Working example notebooks.

---
+++ marmot/datahelpers/helpers.py
@@ -0,0 +1,225 @@
+# Function to calculate total VRE available
+# Function to calculate Total VRE generated
+import pandas as pd
+import json
+import os
+
+
+def get_gen_color_map(file):
+    """
+        Opens a .css file and parses it to get a color mapping.
+        Open the css file in VSCode to quickly update mapping with built in Color Wheel.
+    """
+    color_dict = {}
+    with open(file, 'r') as f:
+
+        for line in f:
+
+            temp = line.strip().split(' ')
+            gen_type = temp[0].replace("#","")
+            color = temp[1].split(':')[1].replace("}","")
+            color_dict[gen_type] = color
+    return color_dict
+
+
+def scale_up_down(df):
+
+    max_val = df.max().max() # Get max value of dataframe
+
+    return df
+
+
+#color_dict = get_gen_color_map("colors.css")
+MW_to_TW = 1000000
+MW_to_GW = 1000
+
+{"start_unit":{"final_unit": "multiplication_factor"}}
+{"MW":{"GW":1000},
+ "MW":{"TW":1000000},
+ "GW":{"MW":1/1000},
+ "GW":{"TW":1000},
+ "TW":{"GW":1/1000},
+ "TW":{"MW":1/1000000}
+}
+
+#columns_ordered = ['Other','Nuclear', 'Coal', 'Gas', 'Hydro','Wind','Offshore-Wind','PV','Battery', 'Curtailment']
+#colors = [color_dict[col] for col in columns_ordered]
+
+# minimum requirements
+
+# mapping to standard technology types
+# TODO remove this dependence?
+#tech_map_simple = json.loads(open("tech_map_simple.json", 'r').read())
+
+#curt_tech = ['WS', 'WT', 'PVe']
+curt_tech = ['Wind',"Offshore-Wind", 'PV']
+
+
+# TODO add labels for each column level. (entity, technology, generator) -> group by entity/technology or just technology.
+
+def get_map_from_file(file_path):
+    """
+        Gets map from json file or python dictionary. Allows user to pass dictionary objects
+        or paths to json files for generating maps required for other data apis.
+    """
+    map = json.loads(open("tech_map_simple.json", 'r').read())
+
+
+    return map
+
+
+def get_raw_generators(scenario_dir):
+
+    gen_path = os.path.normpath(f'{scenario_dir}/generation_actual.pq.gz')
+    #option to read from raw h5 files
+    #TODO track down negative gas generation
+    df = pd.read_parquet(gen_path).applymap(lambda x: x if x >=0.0 else 0.0)
+    return df
+
+def get_raw_availability(scenario_dir):
+
+    avail_path = os.path.normpath(f'{scenario_dir}/generation_availability.pq.gz')
+    #option to read from raw h5 files
+    df = pd.read_parquet(avail_path).applymap(lambda x: x if x >=0.0 else 0.0)
+
+    return df
+
+
+def get_generators_tech(scenario_dir, tech_map):
+
+    gen_df = get_raw_generators(scenario_dir)
+
+    # TODO replace simple tech map with standard EIA tech map
+    gen_df.columns = pd.MultiIndex.from_tuples([(tech_map_simple[tech_map[col]], col) for col in gen_df.columns])
+
+    return gen_df
+
+def get_availability_tech(scenario_dir, tech_map):
+
+    avail_df = get_raw_availability(scenario_dir)
+
+    # TODO replace simple tech map with standard EIA tech map
+    avail_df.columns = pd.MultiIndex.from_tuples([(tech_map_simple[tech_map[col]], col) for col in avail_df.columns])
+
+    return avail_df
+
+
+
+def calc_curtailment(gen_tech, avail_tech):
+
+    curt_gen = pd.concat([avail_tech[curt_tech], -1*gen_tech[curt_tech]]).groupby(level=0).sum()
+    # TODO track down negative curtailment
+    return curt_gen.applymap(lambda x: x if x>=0.0 else 0.0)
+
+
+def get_gen_and_curtailment(scenario_dir, tech_map):
+
+    gen_tech = get_generators_tech(scenario_dir, tech_map)
+    avail_tech = get_availability_tech(scenario_dir, tech_map)
+
+    curt_tech = calc_curtailment(gen_tech, avail_tech)
+
+    curt_tech.columns = pd.MultiIndex.from_tuples([("Curtailment", col[1]) for col in curt_tech.columns])
+
+    return pd.merge(gen_tech, curt_tech, left_index=True, right_index=True)
+
+
+# aggregates across generators
+def get_entity_tech_aggregates(scenario_dir, tech_map, entity_map):
+
+    gen_curt_tech = get_gen_and_curtailment(scenario_dir, tech_map)
+
+    gen_curt_tech.columns = pd.MultiIndex.from_tuples([(entity_map[col[1]], col[0],col[1]) for col in gen_curt_tech.columns])
+
+    return gen_curt_tech
+
+
+
+
+def get_regional_load(scenario_dir):
+
+    return pd.read_parquet(f'{scenario_dir}/regional_load.pq.gz')
+
+def get_entity_load(scenario_dir, entity_map):
+
+    df = get_regional_load(scenario_dir)
+
+    df.columns = pd.MultiIndex.from_tuples([
+        (entity_map[str(col)], str(col)) if col in entity_map.keys() else ("other", str(col)) for col in df.columns
+
+        ])
+
+    return df
+
+
+def get_entity_tech_load_aggregates(scenario_dir, tech_map, gen_entity_map, region_entity_map):
+
+    regional_load = get_entity_load(scenario_dir, region_entity_map)
+    regional_load_agg = regional_load.groupby(axis=1, level=0).sum()
+    regional_load_agg.columns = pd.MultiIndex.from_tuples([(col, "Demand") for col in regional_load_agg.columns])
+
+    gen_tech_load = get_entity_tech_aggregates(scenario_dir, tech_map, gen_entity_map)
+
+    gen_tech_load_agg = gen_tech_load.groupby(axis=1, level=[0,1]).sum()
+
+    gen_tech_load_entity = pd.merge(regional_load_agg, gen_tech_load_agg, left_index=True, right_index=True).sort_index(axis=1)
+    gen_tech_load_entity.attrs['units'] = 'MW'
+
+    return gen_tech_load_entity
+
+
+## Get line flow data
+
+
+def get_line_flow_data(scenario_dir):
+    """
+        Get the raw line flow data for a given SIIP scenario
+    """
+
+    df = pd.read_parquet(f"{scenario_dir}/power_flow_actual.pq.gz")
+
+    return df
+
+
+
+def get_line_load_data(scenario_dir, line_rating_map):
+    """
+    Gets the raw line flow data and normalizes each value to the corresponding
+    Line rating. Requires dictionary/json file of
+
+    """
+
+    flow = get_line_flow_data(scenario_dir)
+
+    loading = pd.DataFrame({col: abs(flow[col]/line_rating_map[col]) for col in flow.columns.values }, index=flow.index)
+
+    #TODO should we have allow for line load to be calculated from peak load instead of dictionary map
+    return loading
+
+
+def get_line_utilization(scenario_dir, line_ratings, threshold=95):
+    """
+        Gets the U99, U95, U90, etc line load rating. Threshold can be set
+        to get desired flags.
+        Returns dataframe of bools/ints
+    """
+
+    return
+
+
+# Level 0 - Geographic Entity (region, zone, interconnect etc.) -> required for region+tech aggs
+# Level 1 - Technology Type -> required for tech aggs
+# Level 2 - Generator ->
+
+# raw generator, availability datasets
+    # -> generator-tech, availability-tech -> generator-tech-curtailment
+    #
+
+
+
+
+
+
+
+
+
---
+++ marmot/scenariohandlers/config_maps.py
@@ -0,0 +1,104 @@
+tech_map_simple = {
+    "BA":"Battery",
+    "CC_NATURAL_GAS":"Gas-CC",
+    "CT_COAL":"Coal",
+    "CT_DISTILLATE_FUEL_OIL":"Oil",
+    "CT_NATURAL_GAS": "Gas-CT",
+    "CT_OTHER": "Other",
+    "CT_RESIDUAL_FUEL_OIL": "Oil",
+    "CT_WASTE_OIL": "Oil",
+    "HY":"Hydro",
+    "IC_DISTILLATE_FUEL_OIL":"Oil",
+    "IC_NATURAL_GAS":"Gas",
+    "OT":"Other",
+    "OT_COAL":"Coal",
+    "OT_MUNICIPAL_WASTE":"Landfill-Gas",
+    "OT_NATURAL_GAS":"Gas",
+    "OT_NUCLEAR":"Nuclear",
+    "OT_OTHER":"Other",
+    "OT_WASTE_COAL":"Coal",
+    "PVe":"PV",
+    "ST_COAL":"Coal",
+    "ST_DISTILLATE_FUEL_OIL":"Oil",
+    "ST_NATURAL_GAS":"Gas",
+    "ST_NUCLEAR":"Nuclear",
+    "ST_OTHER":"Other",
+    "ST_RESIDUAL_FUEL_OIL":"Oil",
+    "ST_WASTE_COAL":"Coal",
+    "ST_WASTE_OIL":"Oil",
+    "WS":"Offshore-Wind",
+    "WT":"Wind"
+}
+
+
+
+egret_map_simple = {
+    'Oil':'Oil',
+    'NG':'Gas',
+    'Coal':'Coal',
+    'Solar':'PV',
+    'Wind':'Wind',
+    'Sync_Cond':'Other',
+    'Nuclear':'Nuclear',
+    'Hydro':'Hydro'
+}
+
+
+plexos_map_simple = {
+
+    'ReEDS_battery': 'Battery',
+    'ReEDS_biopower': 'Biopower',
+    'ReEDS_coal-igcc_coal-ccs_mod': 'Coal',
+    'ReEDS_coal-new_coal-ccs_mod':'Coal',
+    'ReEDS_coaloldscr_coal-ccs_mod':'Coal',
+    'ReEDS_distpv':'PV',
+    'ReEDS_dupv':'dPV',
+    'ReEDS_gas-cc_gas-cc-ccs_mod':'Gas-CC',
+    'ReEDS_hyded':'Hydro',
+    'ReEDS_hydud':'Hydro',
+    'ReEDS_hydend':'Hydro',
+    'ReEDS_lfill-gas':'Landfill-Gas',
+    'ReEDS_gas-cc_re-cc':'Gas-CC',
+    'ReEDS_gas-ct_re-ct':'Gas-CT',
+    'ReEDS_re-cc':'RE-CC',
+    'ReEDS_upv':'PV',
+    'ReEDS_wind-ons':'Wind',
+    'nuclear':'Nuclear',
+    'ReEDS_can-imports':'Imports',
+    'ReEDS_hydund':'Hydro',
+    'ReEDS_pumped-hydro':'Storage',
+    'ReEDS_re-ct':'RE-CT',
+    'ReEDS_wind-ofs':'Offshore-Wind',
+    'ReEDS_beccs_mod':'BECCS',
+    'ReEDS_geothermal':'Geothermal',
+    'ReEDS_gas-cc-ccs_mod':'Gas',
+    'caes':'Storage',
+    'biopower':'Biopower',
+    'coalolduns':'Coal',
+    'ReEDS_nuclear':'Nuclear',
+    'gas-cc':'Gas-CC',
+    'gas-ct':'Gas-CT',
+    'ReEDS_gas-cc':'Gas-CC',
+    'ReEDS_gas-ct':'Gas-CT',
+    'coaloldscr':'Coal',
+    'o-g-s':'Oil-Gas-Steam',
+    'ReEDS_o-g-s':'Oil-Gas-Steam',
+    'ReEDS_coal-new':'Coal',
+    'ReEDS_coaloldscr':'Coal',
+    'coal-new':'Coal',
+    'ReEDS_csp-ns':'Other',
+    'coal-igcc':'Coal',
+    'ReEDS_coalolduns_coal-ccs_mod':'Coal',
+    'ReEDS_dupv':'dPV'
+}
+
+
+volt_line_width={
+    220.0:0.75,
+    230.0:0.75,
+    315.0:1,
+    345.0:1,
+    500.0:1.25,
+    765.0:1.5,
+    735.0:1.5
+}
